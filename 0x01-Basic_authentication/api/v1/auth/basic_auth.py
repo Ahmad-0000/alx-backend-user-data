b12f33ce024280e578357a996ee4db14ba817028
Ahmad <ahmad.new.m.v@gmial.com>
2024-11-18 10:11:02 +0200

    A new authentication mechanism

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication Module
+"""
+from api.v1.auth.auth import Auth
+from os import getenv
+
+
+class BasicAuth(Auth):
+    """To fully implemented later"""
+    pass
