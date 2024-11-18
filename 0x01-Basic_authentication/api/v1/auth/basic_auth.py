a26eec9a9b799edab44c2edbdb769b801c6219e7
Ahmad <ahmad.new.m.v@gmial.com>
2024-11-18 10:49:43 +0200

    Decodes credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Authentication Module
 """
 from api.v1.auth.auth import Auth
 from os import getenv
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,4 +21,18 @@ class BasicAuth(Auth):
         try:
             return authorization_header.split()[1]
         except IndexError:
+            return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes credentials encoded in base64
+        """
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header).\
+                    decode('utf-8')
+        except base64.binascii.Error:
             return None
