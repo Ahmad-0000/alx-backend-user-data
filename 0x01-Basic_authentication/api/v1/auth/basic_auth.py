127856a8c7e026e44e70dc81315356c66804395d
Ahmad <ahmad.new.m.v@gmial.com>
2024-11-18 10:36:52 +0200

    Extracting encoded credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,5 +7,17 @@ from os import getenv
 
 
 class BasicAuth(Auth):
-    """To fully implemented later"""
-    pass
+    """Basic Authentication Mechanism Class"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extracts the base64-encoded username:password"""
+        if not authorization_header:
+            return
+        if type(authorization_header) is not str:
+            return
+        if not authorization_header.startswith("Basic "):
+            return
+        try:
+            return authorization_header.split()[1]
+        except IndexError:
+            return None
