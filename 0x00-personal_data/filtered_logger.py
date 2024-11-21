559e216bdd0d0a7e59872c8f94abcb2bb234ef73
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-21 11:49:55 +0000

    Added type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,9 @@
 Obfuscation
 """
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscation 'fileds' values in 'message'"""
     for field in fields:
         message = re.sub(f'{field}=[^{separator}]*', f'{field}={redaction}', message)
