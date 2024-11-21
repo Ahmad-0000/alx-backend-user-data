63cdc3953554a515d957189821d25f05b9b02b5b
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-21 21:34:02 +0000

    2nd task's solution

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,13 +3,35 @@
 Obfuscation
 """
 import re
-from typing import List
+import logging
+from typing import List, Iterable
 
 
-def filter_datum(fields: List[str], redaction: str,
+
+def filter_datum(fields: Iterable[str], redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscation 'fileds' values in 'message'"""
     for field in fields:
         message = re.sub(f'{field}=[^{separator}]*',
                          f'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: tuple):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formation and obfuscation"""
+        record.msg = filter_datum(self.fields, '***', record.msg, ';')
+        output = logging.Formatter.format(self, record)
+        return output
+    
