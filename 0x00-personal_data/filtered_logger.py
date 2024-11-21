841168ee9af16e1f3797a67738afb610c6285b00
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-21 11:52:01 +0000

    Conformed to pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,11 @@ Obfuscation
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Obfuscation 'fileds' values in 'message'"""
     for field in fields:
-        message = re.sub(f'{field}=[^{separator}]*', f'{field}={redaction}', message)
+        message = re.sub(f'{field}=[^{separator}]*',
+                         f'{field}={redaction}', message)
     return message
